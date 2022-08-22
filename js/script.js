8858e568f96b2b99094520435730d482f5a52071
Pavel <pavel-zh@inbox.ru>
2022-08-22 16:19:35 +0300

    add files

--- js/script.js
+++ js/script.js
@@ -3,116 +3,172 @@
 // Структура goods сделана в Mocky - https://run.mocky.io/v3/afd8d1e8-5507-4c20-a26e-7cc09030f768
 const BASE = 'https://run.mocky.io/v3';
 const GOODS = '/afd8d1e8-5507-4c20-a26e-7cc09030f768';
+const url = `${BASE}${GOODS}`
+
+const app = new Vue({
+    el: "#app",
+
+    data: {
+        items: [],
+        dataState: false,
+        showBasket: false,
+    },
+
+    computed: {
+        // общая стоимость товаров в каталоге
+        totalPriceItems() {
+            return this.items.reduce((prev, { price }) => {
+                let price_valid = price ? parseFloat(price) : 0;
+                return prev + price_valid;
+            }, 0)
+
+        },
+    },
 
 
-// ---- Service переделан на fetch
+    methods: {
+        // полный url для изображения
+        imgUrl(file_name) {
+            return file_name ? `img/${file_name}` : `img/default.jpg`;
+        },
 
-// Пример короткой записи
-// const service = (url) => fetch(url).then((response) => response.json());
-
-
-const service = (url) => fetch(url).then((response) => {
-    // Добавил проверку на успешность запроса (с cors не работает)
-    if (!response.ok) {
-        throw new Error('Ошибка получения данных');
-    }
-    return response.json();
-}).catch((err) => {
-    console.log(err);
-});
+        // форматирование стоимости товара при выводе
+        formatPrice(price) {
+            return new Intl.NumberFormat("ru-RU", {
+                minimumFractionDigits: 0
+            }).format(price);
+        }
+    },
 
 
-// Класс, реализующий ренедринг товара карточки товара
-class GoodsItem {
-    constructor({ id, category = 'не определена', title = 'не добавлен', price = 0, currency, img = 'default.jpg' }) {
-        this.id = id;
-        this.category = category;
-        this.title = title;
-        this.price = price;
-        this.currency = currency;
-        this.img = img;
-    }
+    mounted() {
+        // Получаем данные по основным товарам в каталоге
+        // Сделаем задерку для вывода сообщения
+        setTimeout(() => {
+            fetch(url).then((response) => response.json())
+                .then((data) => {
+                    this.items = data;
+                    this.dataState = true;
+                });
+        }, 1000);
 
-    render() {
-        return `
-        <div class="item">
-            <div class="img_block">
-                <img src="img/${this.img}" alt="${this.title}">   
-            </div>
-            <span class="category_name">${this.category}</span>
-            <a href="#" class="title_for_item" id=${this.id}>${this.title}</a>
-            <span class="price_item">${new Intl.NumberFormat("ru-RU", { minimumFractionDigits: 0 }).format(this.price)} ${this.currency}</span>
-            <button class="btn btn_item" id=${this.id}>В корзину</button>
-        </div>`;
-    }
-}
+    },
 
-
-// Класс, реализующий вывод списка товаров
-class GoodsList {
-    constructor() {
-        this.goods = [];
-        this.filteredGoods = [];
-
-    }
-
-    fetchGoods() {
-        return new Promise((resolve) => {
-            service(`${BASE}${GOODS}`).then((data) => {
-                this.goods = data;
-                this.filteredGoods = data;
-                resolve();
-            }, (error) => {
-                alert(error);
-            })
-        });
-    }
-
-
-    filterGoods(value) {
-        // Фильтруем список товаров по данным из поля
-        const regexp = new RegExp(value, 'i');
-        this.filteredGoods = this.goods.filter(good => regexp.test(good.title));
-        this.render();
-    }
-
-
-    // метод определяет общую стоимость товаров в каталоге
-    calculateСostGoods() {
-        let res_calculate = 0
-        // Вариант решения с деструктуризацией
-        return this.goods.reduce((prev, { price }) => {
-            let price_valid = price ? parseFloat(price) : 0;
-            return prev + price_valid;
-        }, 0)
-    }
-
-    render() {
-        const items = this.filteredGoods.map(good => {
-            const goodItem = new GoodsItem(good);
-            return goodItem.render();
-        }).join('')
-        document.querySelector('.goods-list').innerHTML = items;
-    }
-}
-
-
-// Выводим данные на страницу
-const goodsList = new GoodsList();
-
-goodsList.fetchGoods().then(() => {
-    goodsList.render();
 })
 
+// // Структура goods сделана в Mocky - https://run.mocky.io/v3/afd8d1e8-5507-4c20-a26e-7cc09030f768
+// const BASE = 'https://run.mocky.io/v3';
+// const GOODS = '/afd8d1e8-5507-4c20-a26e-7cc09030f768';
 
-// Вывод суммы по всем товарам каталога
-// alert(`Общая стоимость всех товаров в каталоге составляет: ${list.calculateСostGoods()}`);
 
-// --- ДЗ №4
-const searchButton = document.querySelector('.search-button');
-const searchField = document.querySelector('.goods-search');
-searchButton.addEventListener('click', () => {
-    if (searchField.value != null) {
-        goodsList.filterGoods(searchField.value);
-    }
-});
+// // ---- Service переделан на fetch
+
+// // Пример короткой записи
+// // const service = (url) => fetch(url).then((response) => response.json());
+
+
+// const service = (url) => fetch(url).then((response) => {
+//     // Добавил проверку на успешность запроса (с cors не работает)
+//     if (!response.ok) {
+//         throw new Error('Ошибка получения данных');
+//     }
+//     return response.json();
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+
+// // Класс, реализующий ренедринг товара карточки товара
+// class GoodsItem {
+//     constructor({ id, category = 'не определена', title = 'не добавлен', price = 0, currency, img = 'default.jpg' }) {
+//         this.id = id;
+//         this.category = category;
+//         this.title = title;
+//         this.price = price;
+//         this.currency = currency;
+//         this.img = img;
+//     }
+
+//     render() {
+//         return `
+//         <div class="item">
+//             <div class="img_block">
+//                 <img src="img/${this.img}" alt="${this.title}">
+//             </div>
+//             <span class="category_name">${this.category}</span>
+//             <a href="#" class="title_for_item" id=${this.id}>${this.title}</a>
+//             <span class="price_item">${new Intl.NumberFormat("ru-RU", { minimumFractionDigits: 0 }).format(this.price)} ${this.currency}</span>
+//             <button class="btn btn_item" id=${this.id}>В корзину</button>
+//         </div>`;
+//     }
+// }
+
+
+// // Класс, реализующий вывод списка товаров
+// class GoodsList {
+//     constructor() {
+//         this.goods = [];
+//         this.filteredGoods = [];
+
+//     }
+
+//     fetchGoods() {
+//         return new Promise((resolve) => {
+//             service(`${BASE}${GOODS}`).then((data) => {
+//                 this.goods = data;
+//                 this.filteredGoods = data;
+//                 resolve();
+//             }, (error) => {
+//                 alert(error);
+//             })
+//         });
+//     }
+
+
+//     filterGoods(value) {
+//         // Фильтруем список товаров по данным из поля
+//         const regexp = new RegExp(value, 'i');
+//         this.filteredGoods = this.goods.filter(good => regexp.test(good.title));
+//         this.render();
+//     }
+
+
+//     // метод определяет общую стоимость товаров в каталоге
+//     calculateСostGoods() {
+//         let res_calculate = 0
+//         // Вариант решения с деструктуризацией
+//         return this.goods.reduce((prev, { price }) => {
+//             let price_valid = price ? parseFloat(price) : 0;
+//             return prev + price_valid;
+//         }, 0)
+//     }
+
+//     render() {
+//         const items = this.filteredGoods.map(good => {
+//             const goodItem = new GoodsItem(good);
+//             return goodItem.render();
+//         }).join('')
+//         document.querySelector('.goods-list').innerHTML = items;
+//     }
+// }
+
+
+// // Выводим данные на страницу
+// const goodsList = new GoodsList();
+
+// goodsList.fetchGoods().then(() => {
+//     goodsList.render();
+// })
+
+
+// // Вывод суммы по всем товарам каталога
+// // alert(`Общая стоимость всех товаров в каталоге составляет: ${list.calculateСostGoods()}`);
+
+// // --- ДЗ №4
+// const searchButton = document.querySelector('.search-button');
+// const searchField = document.querySelector('.goods-search');
+// searchButton.addEventListener('click', () => {
+//     if (searchField.value != null) {
+//         goodsList.filterGoods(searchField.value);
+//     }
+// });
